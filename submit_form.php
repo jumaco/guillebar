3415c1ce71cbebb512370ef4e407946ab9f2b7c6
Juan Corral <juan.manuel.corral@hotmail.com>
2021-05-18 00:52:03 -0300

    submit-form

--- submit_form.php
+++ submit_form.php
@@ -1,9 +1,9 @@
 <?php
 //Import PHPMailer classes into the global namespace
 //These must be at the top of your script, not inside a function
-//use PHPMailer\PHPMailer\PHPMailer;
-//use PHPMailer\PHPMailer\SMTP;
-//use PHPMailer\PHPMailer\Exception;
+use PHPMailer\PHPMailer\PHPMailer;
+use PHPMailer\PHPMailer\SMTP;
+use PHPMailer\PHPMailer\Exception;
 
 //Load Composer's autoloader
 require 'vendor/autoload.php';
@@ -35,54 +35,13 @@ try {
     //$mail->addAttachment('/tmp/image.jpg', 'new.jpg');    //Optional name
 
     //Content
-/*    $mail->isHTML(true);                                  //Set email format to HTML
+    $mail->isHTML(true);                                  //Set email format to HTML
     $mail->Subject = 'Consulta de: '.$_POST['inputName'];
     $mail->Body    = 'Nombre: '.$_POST['inputName'].'<br>Mensaje: '.$_POST['inputMessage'].'<br>Email: '.$_POST['inputEmail'];
     //$mail->AltBody = 'This is the body in plain text for non-HTML mail clients';
 
     $mail->send();
     header("location: gracias.html");
-} catch (Exception $e) {
-    //echo "Message could not be sent. Mailer Error: {$mail->ErrorInfo}";
-    header("location: error.html");
-}*/
-
-
-
-if(isset($_POST['contactFrmSubmit']) && !empty($_POST['name']) && !empty($_POST['email']) && (!filter_var($_POST['email'], FILTER_VALIDATE_EMAIL) === false) && !empty($_POST['message'])){
-    
-    // Submitted form data
-    $name   = $_POST['name'];
-    $email  = $_POST['email'];
-    $message= $_POST['message'];
-    
-    /*
-     * Send email to admin
-     */
-    $to     = 'admin@example.com';
-    $subject= 'Contact Request Submitted';
-    
-    $htmlContent = '
-    <h4>Contact request has submitted at CodexWorld, details are given below.</h4>
-    <table cellspacing="0" style="width: 300px; height: 200px;">
-        <tr>
-            <th>Name:</th><td>'.$name.'</td>
-        </tr>
-        <tr style="background-color: #e0e0e0;">
-            <th>Email:</th><td>'.$email.'</td>
-        </tr>
-        <tr>
-            <th>Message:</th><td>'.$message.'</td>
-        </tr>
-    </table>';
-    
-    // Set content-type header for sending HTML email
-    $headers = "MIME-Version: 1.0" . "rn";
-    $headers .= "Content-type:text/html;charset=UTF-8" . "rn";
-    
-    // Additional headers
-    $headers .= 'From: CodexWorld<sender@example.com>' . "rn";
-    
     // Send email
     if(mail($to,$subject,$htmlContent,$headers)){
         $status = 'ok';
@@ -93,7 +52,5 @@ if(isset($_POST['contactFrmSubmit']) && !empty($_POST['name']) && !empty($_POST[
     // Output status
     echo $status;die;
 }
-
-
 
 ?>
