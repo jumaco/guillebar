f1de265f1255fd9c01a473c944ecbe255de62a0a
Juan Corral <juan.manuel.corral@hotmail.com>
2021-05-17 21:23:12 -0300

    phpMailer

---
+++ email.php
@@ -0,0 +1,50 @@
+<?php
+//Import PHPMailer classes into the global namespace
+//These must be at the top of your script, not inside a function
+use PHPMailer\PHPMailer\PHPMailer;
+use PHPMailer\PHPMailer\SMTP;
+use PHPMailer\PHPMailer\Exception;
+
+//Load Composer's autoloader
+require 'vendor/autoload.php';
+
+//Instantiation and passing `true` enables exceptions
+$mail = new PHPMailer(true);
+
+try {
+    //Server settings
+    $mail->SMTPDebug = SMTP::DEBUG_SERVER;                      //Enable verbose debug output
+    $mail->isSMTP();                                            //Send using SMTP
+    $mail->Host       = 'smtp.gmail.com';                     //Set the SMTP server to send through
+    $mail->SMTPAuth   = true;                                   //Enable SMTP authentication
+    $mail->Username   = 'front.fullstack@gmail.com';                     //SMTP username
+    $mail->Password   = 'Front2021';                         //SMTP password
+    $mail->SMTPSecure = PHPMailer::ENCRYPTION_SMTPS;         //Enable TLS encryption; `PHPMailer::ENCRYPTION_SMTPS` encouraged
+    $mail->Port       = 465;                                    //TCP port to connect to, use 465 for `PHPMailer::ENCRYPTION_SMTPS` above
+
+    //Recipients
+    $mail->setFrom('front.fullstack@gmail.com', 'LoDeFuilleBar'); // Hacer coincidir con el username. (preferentemente)
+    $mail->addAddress('juan.manuel.corral@hotmail.com', 'Juan');     //Add a recipient
+    //$mail->addAddress('ellen@example.com');               //Name is optional
+    //$mail->addReplyTo('info@example.com', 'Information');
+    //$mail->addCC('cc@example.com');
+    //$mail->addBCC('bcc@example.com');
+
+    //Attachments
+    //$mail->addAttachment('/var/tmp/file.tar.gz');         //Add attachments
+    //$mail->addAttachment('/tmp/image.jpg', 'new.jpg');    //Optional name
+
+    //Content
+    $mail->isHTML(true);                                  //Set email format to HTML
+    $mail->Subject = 'Consulta de: '.$_POST['inp_nombre'];
+    $mail->Body    = 'Nombre: '.$_POST['inp_nombre'].'<br>De dónde nos conoció: '.$_POST['inp_nexo'].'<br>Mensaje: '.$_POST['inp_mensaje'].'<br>Email: '.$_POST['inp_email'];
+    //$mail->AltBody = 'This is the body in plain text for non-HTML mail clients';
+
+    $mail->send();
+    header("location: gracias.html");
+} catch (Exception $e) {
+    //echo "Message could not be sent. Mailer Error: {$mail->ErrorInfo}";
+    header("location: error.html");
+}
+
+?>
