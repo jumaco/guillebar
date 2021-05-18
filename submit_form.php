69dbdd9bd510bc546cd68740bf10e34696588fc2
Juan Corral <juan.manuel.corral@hotmail.com>
2021-05-17 23:36:02 -0300

    modal-Ajax

---
+++ submit_form.php
@@ -0,0 +1,103 @@
+<?php
+//Import PHPMailer classes into the global namespace
+//These must be at the top of your script, not inside a function
+use PHPMailer\PHPMailer\PHPMailer;
+use PHPMailer\PHPMailer\SMTP;
+use PHPMailer\PHPMailer\Exception;
+
+//Load Composer's autoloader
+require 'vendor/autoload.php';
+
+//Instantiation and passing `true` enables exceptions
+$mail = new PHPMailer(true);
+
+try {
+    //Server settings
+    $mail->SMTPDebug = SMTP::DEBUG_SERVER;                      //Enable verbose debug output
+    $mail->isSMTP();                                            //Send using SMTP
+    $mail->Host       = 'smtp.gmail.com';                     //Set the SMTP server to send through
+    $mail->SMTPAuth   = true;                                   //Enable SMTP authentication
+    $mail->Username   = 'front.fullstack@gmail.com';                     //SMTP username
+    $mail->Password   = 'Front2021';                         //SMTP password
+    $mail->SMTPSecure = tls;         //Enable TLS encryption; `PHPMailer::ENCRYPTION_SMTPS` encouraged
+    $mail->Port       = 587;                                    //TCP port to connect to, use 465 for `PHPMailer::ENCRYPTION_SMTPS` above
+
+    //Recipients
+    $mail->setFrom('front.fullstack@gmail.com', 'LoDeFuilleBar'); // Hacer coincidir con el username. (preferentemente)
+    $mail->addAddress('juan.manuel.corral@hotmail.com', 'Juan');     //Add a recipient
+    //$mail->addAddress('ellen@example.com');               //Name is optional
+    //$mail->addReplyTo('info@example.com', 'Information');
+    //$mail->addCC('cc@example.com');
+    //$mail->addBCC('bcc@example.com');
+
+    //Attachments
+    //$mail->addAttachment('/var/tmp/file.tar.gz');         //Add attachments
+    //$mail->addAttachment('/tmp/image.jpg', 'new.jpg');    //Optional name
+
+    //Content
+    $mail->isHTML(true);                                  //Set email format to HTML
+    $mail->Subject = 'Consulta de: '.$_POST['inputName'];
+
+
+
+/*    $mail->Body    = 'Nombre: '.$_POST['inputName'].'<br>Mensaje: '.$_POST['inputMessage'].'<br>Email: '.$_POST['inputEmail'];
+    //$mail->AltBody = 'This is the body in plain text for non-HTML mail clients';
+
+    $mail->send();
+    header("location: gracias.html");
+} catch (Exception $e) {
+    //echo "Message could not be sent. Mailer Error: {$mail->ErrorInfo}";
+    header("location: error.html");
+}
+*/
+
+
+
+if(isset($_POST['contactFrmSubmit']) && !empty($_POST['name']) && !empty($_POST['email']) && (!filter_var($_POST['email'], FILTER_VALIDATE_EMAIL) === false) && !empty($_POST['message'])){
+    
+    // Submitted form data
+    $name   = $_POST['name'];
+    $email  = $_POST['email'];
+    $message= $_POST['message'];
+    
+    /*
+     * Send email to admin
+     */
+//    $to     = 'admin@example.com';
+//    $subject= 'Contact Request Submitted';
+    
+    $htmlContent = '
+    <h4>Contact request has submitted at CodexWorld, details are given below.</h4>
+    <table cellspacing="0" style="width: 300px; height: 200px;">
+        <tr>
+            <th>Name:</th><td>'.$name.'</td>
+        </tr>
+        <tr style="background-color: #e0e0e0;">
+            <th>Email:</th><td>'.$email.'</td>
+        </tr>
+        <tr>
+            <th>Message:</th><td>'.$message.'</td>
+        </tr>
+    </table>';
+    
+    // Set content-type header for sending HTML email
+    $headers = "MIME-Version: 1.0" . "rn";
+    $headers .= "Content-type:text/html;charset=UTF-8" . "rn";
+    
+    // Additional headers
+    $headers .= 'From: CodexWorld<sender@example.com>' . "rn";
+    
+    // Send email
+    if(mail($to,$subject,$htmlContent,$headers)){
+        $status = 'ok';
+    }else{
+        $status = 'err';
+    }
+    
+    // Output status
+    echo $status;die;
+}
+
+
+
+?>
