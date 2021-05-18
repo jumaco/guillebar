939592b5587c85cb1e9ceb54f391307d99f8485b
Juan Corral <juan.manuel.corral@hotmail.com>
2021-05-18 05:03:14 -0300

    ModSubmit5

--- submit_form.php
+++ submit_form.php
@@ -1,26 +1,25 @@
 <?php
-require 'mailer/PHPMailerAutoload.php';
+//Import PHPMailer classes into the global namespace
+//These must be at the top of your script, not inside a function
+use PHPMailer\PHPMailer\PHPMailer;
+use PHPMailer\PHPMailer\SMTP;
+use PHPMailer\PHPMailer\Exception;
 
-$mail = new PHPMailer;
+//Load Composer's autoloader
+require 'vendor/autoload.php';
 
-// Form Data
-$fullname = $_POST['fullname'] ;
-$email = $_POST['email'] ;
-$message = $_POST['message'] ;
-
-
-$mail->isSMTP();                                      // Set mailer to use SMTP
-$mail->Host = 'smtp.gmail.com'; // Specify main and backup SMTP servers
-$mail->SMTPAuth = true; // Enable SMTP authentication
-$mail->Username = 'front.fullstack@gmail.com'; // SMTP username
-$mail->Password = 'Front2021'; // SMTP password
-$mail->SMTPSecure = 'tls'; // Enable TLS encryption, `ssl` also accepted
-$mail->Port = 587; // TCP port to connect to
-
-$mail->setFrom('front.fullstack@gmail.com', 'LoDeFuilleBar'); // Admin ID
+//Instantiation and passing `true` enables exceptions
+$mail = new PHPMailer(true);
+$mail->SMTPDebug = SMTP::DEBUG_SERVER;                      //Enable verbose debug output
+$mail->isSMTP();                                            //Send using SMTP
+$mail->Host       = 'smtp.gmail.com';                     //Set the SMTP server to send through
+$mail->SMTPAuth   = true;                                   //Enable SMTP authentication
+$mail->Username   = 'front.fullstack@gmail.com';                     //SMTP username
+$mail->Password   = 'Front2021';                         //SMTP password
+$mail->SMTPSecure = PHPMailer::ENCRYPTION_SMTPS;         //Enable TLS encryption; `PHPMailer::ENCRYPTION_SMTPS` encouraged
+$mail->Port       = 465;                                    //TCP port to connect to, use 465 for `PHPMailer::ENCRYPTION_SMTPS` above
+$mail->setFrom('front.fullstack@gmail.com', 'LoDeGuilleBar'); // Admin ID
 $mail->addAddress('juan.manuel.corral@hotmail.com', 'Juan'); // Business Owner ID
-$mail->addReplyTo($email, $fullname); // Form Submitter's ID
-
 $mail->isHTML(true);                                  //Set email format to HTML
 $mail->Subject = 'Consulta de: '.$_POST['fullname'];
 $mail->Body    = 'Nombre: '.$_POST['fullname'].'<br>Mensaje: '.$_POST['message'].'<br>Email: '.$_POST['email'];
